6afa7496f17242a3cf13bd457882d960aa45f40d
bartosz.jakubiak <bartosz.jakubiak@gmail.com>
2024-04-09 10:05:42 +0200

    Set character limit on thread title and comment content.

--- src/main/java/com/bartoszj/forumwithmysql/model/comments/CommentDtoIn.java
+++ src/main/java/com/bartoszj/forumwithmysql/model/comments/CommentDtoIn.java
@@ -1,13 +1,16 @@
 package com.bartoszj.forumwithmysql.model.comments;
 
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 
 public class CommentDtoIn {
     @NotNull(message = "thread ID is required")
     private Long threadId;
     @NotEmpty(message = "comment content is required")
+    @Pattern(regexp = "^[a-zA-Z0-9,.:;<>?!() _-]{0,255}$", message = "Character limit is 255. Please use only letters, digits and ,.:;/<>?!()_-")
     private String content;
 
     public CommentDtoIn() {
--- src/main/java/com/bartoszj/forumwithmysql/model/threads/ThreadDtoIn.java
+++ src/main/java/com/bartoszj/forumwithmysql/model/threads/ThreadDtoIn.java
@@ -1,9 +1,11 @@
 package com.bartoszj.forumwithmysql.model.threads;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 
 public class ThreadDtoIn {
     @NotBlank(message = "title is required")
+    @Pattern(regexp = "^[a-zA-Z0-9,.:;<>?!() _-]{0,255}$", message = "Character limit is 255. Please use only letters, digits and ,.:;/<>?!()_-")
     private String title;
 
     public ThreadDtoIn() {
